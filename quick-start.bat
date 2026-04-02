1fbb88d0536ae3bdab67362c9dd515b1b7bbc3a3
xiao lin <2422371485@qq.com>
2026-04-02 22:16:36 +0800

    1.添加了快速启动的bat(适合已配置好环境，对代码的测试) 2.给登录与注册页面添加了滑动条，避免超出页面的内容无法显示

---
+++ quick-start.bat
@@ -0,0 +1,4 @@
+@echo off
+chcp 65001 >nul
+echo 正在启动理程学院前端项目...
+npm run dev
--- src/views/Login/LoginPage.vue
+++ src/views/Login/LoginPage.vue
@@ -141,6 +141,9 @@ const handleLogin = async () => {
   background: white;
   border-radius: 10px;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+
+  max-height: 90vh;
+  overflow-y: auto;
 }
 
 .logo {
